91139bc02d782a2b1dffc4ebe7c9374588fb9660
utagawal <16464382+utagawal@users.noreply.github.com>
2023-08-12 14:32:52 +0200

    Add faster construction script
    Add faster construction script for dev purpose to avoid DL OSM PBF and focus on style compilation

---
+++ update_map_light.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Update the map with the new mkgmap instructions, without downloading the osm PBF file
+echo "Update of the map $1 ...."
+
+land=$1
+id=$2
+type=$3
+# url=$4
+
+# land_lower=$(echo $land | tr '[:upper:]' '[:lower:]')
+# land_lower=$(echo $land_lower | tr ' ' _ )
+# file="$land_lower".osm.pbf
+# url_poly=${url//-latest.osm.pbf/.poly}
+# file_poly="$land_lower".poly
+
+# cd "carte_$land_lower"
+
+# curl -L -o $file $url
+
+# curl -L -o $file_poly $url_poly
+
+# cd ..
+
+bash create_map.sh "$land" $id $type
+
+
