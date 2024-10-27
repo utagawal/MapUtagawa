7d54df6abdd3d5ec9cb3aba12cf0022dae6ddae6
Lilian Morinon <lilian_morinon@yahoo.fr>
2024-10-27 21:31:08 +0100

    Use 8 CPUS for converting IGN courbes (needs ~16Gb free to run)

--- get_contours.py
+++ get_contours.py
@@ -5,6 +5,8 @@ import subprocess
 from scripts_hgt.get_hgt import get_hgt
 import os, shutil, fnmatch
 import subprocess
+from multiprocessing import Pool
+import glob
 
 class cd:
     """Context manager for changing the current working directory"""
@@ -18,49 +20,76 @@ class cd:
     def __exit__(self, etype, value, traceback):
         os.chdir(self.savedPath)
 
+def process_shp_file(args):
+    """Process a single shapefile with ogr2osm"""
+    shp_file, script_path = args
+    cmd = f'ogr2osm -t {script_path} --add-bounds "{shp_file}"'
+    subprocess.run(cmd, shell=True)
+    # Create gzip file
+    osm_file = shp_file.replace('.shp', '.osm')
+    if os.path.exists(osm_file):
+        subprocess.run(f'gzip -c "{osm_file}" > "{osm_file}.gz"', shell=True)
+        os.remove(osm_file)
+    return shp_file
+
 def get_contours(country_name, url):
     country_name_lower_case = country_name.lower().replace(" ", "_")
 
-    os.makedirs("dem/"+country_name_lower_case, exist_ok=True)
-
-    os.makedirs("carte_"+country_name_lower_case, exist_ok=True)
+    os.makedirs(f"dem/{country_name_lower_case}", exist_ok=True)
+    os.makedirs(f"carte_{country_name_lower_case}", exist_ok=True)
 
     #Get poly file
-    if ("-latest.osm.pbf" in url):
-        url_poly= url.replace("-latest.osm.pbf",".poly")
+    if "-latest.osm.pbf" in url:
+        url_poly = url.replace("-latest.osm.pbf", ".poly")
     else:
-        url_poly= url.replace(".pbf",".poly")
+        url_poly = url.replace(".pbf", ".poly")
 
-    if(os.path.isdir("dem/"+country_name_lower_case+"/ign")):
+    if os.path.isdir(f"dem/{country_name_lower_case}/ign"):
         print("Folder IGN found")
-        with cd("dem/"+country_name_lower_case+"/ign"):
-            path = os.getcwd()
-            #Transform shp to osm
-            subprocess.run("for i in *.shp; do ogr2osm -t ../../../scripts_shp/contour-translation.py --add-bounds $i; done",shell=True)
-            #Transform osm to osm.gz
-            subprocess.run("for i in *.osm; do gzip -c $i > $i.gz; done",shell=True)
-            #Remove .osm
-            subprocess.run("rm *.osm",shell=True)
-        #Move
-        for f in fnmatch.filter(os.listdir("dem/"+country_name_lower_case+"/ign/"), "*.osm.gz"):
-            shutil.move(os.path.join("dem/"+country_name_lower_case+"/ign/", f), os.path.join("carte_"+country_name_lower_case+"/", f))
+        with cd(f"dem/{country_name_lower_case}/ign"):
+            # Get list of shp files
+            shp_files = glob.glob("*.shp")
+            script_path = "../../../scripts_shp/contour-translation.py"
+            
+            # Prepare arguments for parallel processing
+            args = [(shp_file, script_path) for shp_file in shp_files]
+            
+            # Process files in parallel using 8 CPUs
+            with Pool(processes=8) as pool:
+                pool.map(process_shp_file, args)
+
+        # Move generated files
+        for f in fnmatch.filter(os.listdir(f"dem/{country_name_lower_case}/ign/"), "*.osm.gz"):
+            shutil.move(
+                os.path.join(f"dem/{country_name_lower_case}/ign/", f),
+                os.path.join(f"carte_{country_name_lower_case}/", f)
+            )
     else:
         print("HGT")        
-        #Get hgt
+        # Get hgt
         get_hgt(country_name_lower_case, url_poly)
-        #Download
-        if ( os.path.isfile("dem/"+country_name_lower_case+"/hgt_urls.txt")): 
-            subprocess.run(["bash", "scripts_hgt/download_hgt.sh","dem/"+country_name_lower_case+"/hgt_urls.txt"])
-        #Transform
-        with cd("dem/"+country_name_lower_case):
-            path = os.getcwd()
-            subprocess.run("pyhgtmap --step=10 --no-zero-contour --simplifyContoursEpsilon=0 --jobs=16 --gzip=1 *.hgt",shell=True)    
-        #Move
-        for f in fnmatch.filter(os.listdir("dem/"+country_name_lower_case+"/"), "*.osm.gz"):
-            shutil.move(os.path.join("dem/"+country_name_lower_case+"/", f), os.path.join("carte_"+country_name_lower_case+"/", f))
+        
+        # Download
+        if os.path.isfile(f"dem/{country_name_lower_case}/hgt_urls.txt"):
+            subprocess.run(["bash", "scripts_hgt/download_hgt.sh", f"dem/{country_name_lower_case}/hgt_urls.txt"])
+        
+        # Transform
+        with cd(f"dem/{country_name_lower_case}"):
+            # Note: pyhgtmap already supports parallel processing with --jobs=8
+            subprocess.run("pyhgtmap --step=10 --no-zero-contour --simplifyContoursEpsilon=0 --jobs=8 --gzip=1 *.hgt", shell=True)    
+        
+        # Move generated files
+        for f in fnmatch.filter(os.listdir(f"dem/{country_name_lower_case}/"), "*.osm.gz"):
+            shutil.move(
+                os.path.join(f"dem/{country_name_lower_case}/", f),
+                os.path.join(f"carte_{country_name_lower_case}/", f)
+            )
 
 if __name__ == '__main__':
-    country_name=sys.argv[1]
-    url=sys.argv[2]
+    if len(sys.argv) != 3:
+        print("Usage: script.py country_name url")
+        sys.exit(1)
+    
+    country_name = sys.argv[1]
+    url = sys.argv[2]
     get_contours(country_name, url)
-
